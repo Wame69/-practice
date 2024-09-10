f9af7176e7467b83a50d8f91b0c0fa722630ac5f
Wame69 <165180226+Wame69@users.noreply.github.com>
2024-09-10 10:36:41 +0700

    Update Program.cs

--- practica/Program.cs
+++ practica/Program.cs
@@ -378,3 +378,280 @@
 //    Console.Write($"{arr[i]} ");
 //}
 //Console.WriteLine();
+//taski 16
+/*Console.WriteLine("Введите размер матрицы AxB");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int[,] matr = new int[a,b];
+int[,] matr1 = new int[a, b];
+Console.WriteLine("Введите первую матрицу");
+for (int i = 0; i < a; i++)
+{
+    for(int j = 0; j < b; j++)
+    {
+        matr[i,j] = int.Parse(Console.ReadLine());
+    }
+}
+Console.WriteLine("Введите 2 матрицу");
+for (int i = 0; i < a; i++)
+{
+    for (int j = 0; j < b; j++)
+    {
+        matr1[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+Console.WriteLine("Введите операцию * + - транспорирование");
+switch (Console.ReadLine())
+{
+    case "+":
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                matr[i, j] = matr[i, j] + matr1[i, j];
+            }
+        }
+        break;
+    case "-":
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                matr[i, j] = matr[i, j] - matr1[i, j];
+            }
+        }
+        break;
+    case "*":
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                matr[i, j] = matr[i, j] * matr1[i, j];
+            }
+        }
+        break;
+    case "транспорирование":
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                matr1[j, i] = matr[i, j];
+            }
+        }
+        matr = matr1;
+        break;
+}
+for (int i = 0; i < a; i++)
+{
+    for (int j = 0; j < b; j++)
+    {
+        Console.Write(matr[i,j] + " ");
+    }
+    Console.WriteLine(" ");
+}*/
+//task17
+/*Console.WriteLine("Введите ваше 10-ное число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите систему счисления для перевода (2, 8, 16)");
+int choice = int.Parse(Console.ReadLine());
+switch (choice)
+{
+    case 2:
+        string result = Convert.ToString(num, 2);
+        Console.WriteLine(result);
+        break;
+    case 8:
+        string result1 = Convert.ToString(num, 8);
+        Console.WriteLine(result1);
+        break;
+    case 16:
+        string result2 = Convert.ToString(num, 16);
+        Console.WriteLine(result2);
+        break;
+}*/
+//task18
+/*Console.WriteLine("Числовая пирамида");
+int l = 1 + 2 * (10 - 1);
+int n = 1;
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(new string(' ', (int)(l - n / 2)) + new string('1', n));
+    n += 2;
+}*/
+/*//task19
+Console.WriteLine("Введите строку");
+string str = Console.ReadLine();
+Console.WriteLine("Введите подстроку для поиска");
+string substr = Console.ReadLine();
+int index = str.IndexOf(substr);
+if (index == -1)
+{
+    Console.WriteLine("Вхождения не найдено");
+}
+else
+{
+    Console.WriteLine(index);
+}*/
+//task 20
+Console.WriteLine("Удаление пробелов");
+Console.WriteLine("Ваша строка");
+string str = Console.ReadLine();
+while (str.Contains("  ")) 
+{
+    str = str.Replace("  ", " ");
+}
+Console.WriteLine(str);
+//task 21
+/*Console.WriteLine("Введите первую дату (ДД.ММ.ГГГГ):");
+string firstDateInput = Console.ReadLine();
+Console.WriteLine("Введите вторую дату (ДД.ММ.ГГГГ):");
+string secondDateInput = Console.ReadLine();
+DateTime firstDate;
+DateTime secondDate;
+if (DateTime.TryParseExact(firstDateInput, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out firstDate) &&
+    DateTime.TryParseExact(secondDateInput, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out secondDate))
+{
+    TimeSpan difference = secondDate - firstDate;
+    int daysDifference = (int)difference.TotalDays;
+    Console.WriteLine($"Количество дней между датами: {daysDifference}");
+}
+else
+{
+    Console.WriteLine("Некорректный формат даты. Используйте формат ДД.ММ.ГГГГ.");
+}*/
+//task 22
+/*using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+static void CheckPhoneNum()
+{
+    Console.WriteLine("Введите номер телефона (проверка работает только на номера России):");
+    string phoneNumber = Console.ReadLine();
+    if (phoneNumber.Length > 11)
+    {
+        Console.WriteLine("Номер телефона должен состоять из 11 цифр.");
+    }
+    if (phoneNumber.Length == 11 && phoneNumber[0] == '8' && phoneNumber[1] == '9')
+    {
+        bool isNumeric = true;
+        foreach (char c in phoneNumber)
+        {
+            if (!char.IsDigit(c))
+            {
+                isNumeric = false;
+                break;
+            }
+        }
+
+        if (isNumeric)
+        {
+            Console.WriteLine("Номер телефона введен корректно!");
+        }
+        else
+        {
+            Console.WriteLine("Номер телефона должен состоять только из цифр.");
+        }
+    }
+
+}
+static void CheckEmail()
+{
+    Console.WriteLine("Введите вашу эл.почту");
+    string email = Console.ReadLine();
+    if (email.Contains("@") && email.Contains(".") && email.IndexOf('@') > 0 && email.IndexOf('.') > email.IndexOf('@'))
+    {
+        Console.WriteLine("Ваша почта верна");
+    }
+    else
+    {
+        Console.WriteLine("Ваша почта не верна");
+    }
+
+}
+Console.WriteLine("\nВыберите, что вы хотите проверить: ");
+Console.WriteLine("1. Номер телефона");
+Console.WriteLine("2. email");
+int choice = int.Parse(Console.ReadLine());
+switch (choice)
+{
+    case 1:
+        CheckPhoneNum();
+        break;
+    case 2:
+        CheckEmail();
+        break;
+}
+    */
+    /*//task 23
+public class PasswordGenerator
+{
+    public static string GeneratePassword(int length, bool useLowercase, bool useUppercase, bool useNumbers, bool useSpecial)
+    {
+        Random random = new Random();
+        if (length <= 0)
+            throw new ArgumentException("Длина пароля должна быть положительным числом");
+
+        char[] password = new char[length];
+        string charsToUse = "";
+
+        if (useLowercase) charsToUse += "abcdefghijklmnopqrstuvwxyz";
+        if (useUppercase) charsToUse += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if (useNumbers) charsToUse += "0123456789";
+        if (useSpecial) charsToUse += "!@#$%^&*()-_=+";
+
+        for (int i = 0; i < length; i++)
+        {
+            password[i] = charsToUse[random.Next(charsToUse.Length)];
+        }
+
+        return new string(password);
+    }
+    static void Main()
+    {
+        Console.WriteLine("Введите желаемую длину пароля");
+        int length = int.Parse(Console.ReadLine());
+        Console.WriteLine("Далее выбирайте true/false");
+        Console.WriteLine("Использовать маленькие буквы?");
+        bool useLowercase = bool.Parse(Console.ReadLine());
+        Console.WriteLine("Использовать большие буквы?");
+        bool useUppercase = bool.Parse(Console.ReadLine());
+        Console.WriteLine("Использовать цифры?");
+        bool useNumbers = bool.Parse(Console.ReadLine());
+        Console.WriteLine("Использовать специальные символы");
+        bool useSpecial = bool.Parse(Console.ReadLine());
+        string password = PasswordGenerator.GeneratePassword(length, useLowercase, useUppercase, useNumbers, useSpecial);
+        Console.WriteLine("Вот ваш пароль " + password);
+    }
+}
+*/
+//task 24
+void AllSum(List<int> priList)
+{
+    int sum = 0;
+    foreach (int i in priList)
+    {
+        sum =+ i;
+    }
+    Console.WriteLine(sum);
+}
+List<string> purList = new List<string>();
+List<int> priList = new List<int>();
+Console.WriteLine("Вводите ваши покупочки и их цену (в рублях)");
+Console.WriteLine("\n Чтобы закончить, введите 1");
+bool work = true;
+
+    Console.WriteLine("Ваш товар: ");
+    string purchase = Console.ReadLine();
+    purList.Add(purchase);
+    Console.WriteLine("Добавлено: " + purchase);
+    Console.WriteLine("Стоимость товара " + purchase);
+    int price = int.Parse(Console.ReadLine());
+    Console.WriteLine("Стоимость добавлена!");
+Console.WriteLine("Ваш товар: ");
+ purchase = Console.ReadLine();
+purList.Add(purchase);
+Console.WriteLine("Добавлено: " + purchase);
+Console.WriteLine("Стоимость товара " + purchase);
+price = int.Parse(Console.ReadLine());
+Console.WriteLine("Стоимость добавлена!");
+AllSum(priList);
